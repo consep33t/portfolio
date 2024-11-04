8931ec600e554fcc13ef407b1072e473fffc7539
Consep33t <agengp360@gmail.com>
2024-11-05 02:32:24 +0700

    install server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Atur middleware untuk setiap folder yang berisi file statis
+app.use("/components", express.static(path.join(__dirname, "components")));
+app.use("/pages", express.static(path.join(__dirname, "pages")));
+app.use("/script", express.static(path.join(__dirname, "script")));
+app.use("/style", express.static(path.join(__dirname, "style")));
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+// Rute untuk index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Jalankan server
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
+});
